1cb622c48c3d955f9b76384f6ce2b18bfbe19c52
ChihWei Wang <e1205537@gmail.com>
2019-05-16 11:39:11 +0800

    新增MODEL 和 分析 excel class

--- ProteinAccountSystem/Common/Entity/Item.cs
+++ ProteinAccountSystem/Common/Entity/Item.cs
@@ -9,23 +9,50 @@ namespace Common.Entity
 {
     public class Item
     {
+        /// <summary>
+        /// 庫存
+        /// </summary>
         public int Storage { get; set; }
 
+        /// <summary>
+        /// 商品編號
+        /// </summary>
         public int ItemCode { get; set; }
 
+        /// <summary>
+        /// 商品口味
+        /// </summary>
         public FlavorEnum Flavor { get; set; }
 
+        /// <summary>
+        /// 商品品牌
+        /// </summary>
         public BrandEnum Brand { get; set; }
 
+        /// <summary>
+        /// 商品分類
+        /// </summary>
         public ProductionType ProductionType { get; set; }
 
-        public string Name { get; set; }
-
-        public  int  FixedPrice{ get; set; }
+        /// <summary>
+        /// 商品訂價
+        /// </summary>
+        public int NetPrice { get; set; }
 
         /// <summary>
         /// in precentage
         /// </summary>
         public double Discount { get; set; }
+        
+        /// <summary>
+        /// 商品成本
+        /// </summary>
+        public int Cost { get; set; }
+        
+        /// <summary>
+        /// 稅金
+        /// </summary>
+        public int Tax { get; set; }
+        
     }
 }
