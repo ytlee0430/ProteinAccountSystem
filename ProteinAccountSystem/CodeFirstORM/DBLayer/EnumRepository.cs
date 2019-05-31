3db26fa937a0e46d862e4bdb33e7d2777b1089df
ext_brucel <ext_brucel@trendmicro.com>
2019-05-31 19:35:47 +0800

    add enum entity

---
+++ ProteinAccountSystem/CodeFirstORM/DBLayer/EnumRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CodeFirstORM.Entity;
+using Common.Entity;
+using Common.Enum;
+using Newtonsoft.Json;
+
+namespace CodeFirstORM.DBLayer
+{
+    public class EnumRepository : RepositoryBase<EnumEntity>
+    {
+        public EnumRepository(): base(new ProteinDB())
+        {
+        }
+    }
+}
---
+++ ProteinAccountSystem/CodeFirstORM/Entity/EnumEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstORM.RepositoryBase;
+using Common.Enum;
+
+namespace CodeFirstORM.Entity
+{
+    public class EnumEntity : IEntity
+    {
+        [Key]
+        public int Key { get; set; }
+
+        public int EnumClass { get; set; }
+
+        public int EnumValue { get; set; }
+
+        public string Description { get; set; }
+
+        public string KeyWord { get; set; }
+
+    }
+}
